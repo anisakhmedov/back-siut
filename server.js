a6ac2e10a67a0cc9be92f1240bcfd56b19414183
anisakhmedov <akhmedovanis5@gmail.com>
2025-08-03 21:39:18 +0500

    asd

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-// const mongoURI = 'mongodb+srv://akhmedovanis:nipanid2@cluster0.1bp9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.log(err));
+const mongoURI = 'mongodb+srv://akhmedovanis:nipanid2@cluster0.1bp9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-mongoose.connect("mongodb://localhost:27017/university", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose.connect("mongodb://localhost:27017/university", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/students", require("./routes/students"));
 app.use("/api/departments", require("./routes/departments"));
